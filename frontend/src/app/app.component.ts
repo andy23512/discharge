c10b0bbb295b56b243cad0e9994bb307a0eb2e5b
Tangent Chang <andy23512@gmail.com>
2019-05-23 23:42:41 +0800

    Display groups and messages in frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+import { parse } from 'ansicolor';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+
+interface Data {
+  message: string;
+}
+
+interface Chunk {
+  css: SafeStyle;
+  text: string;
+}
 
 interface Message {
-  message: string;
+  group: string;
+  chunks: Chunk[];
+}
+
+interface Group {
+  name: string;
+  css: string;
 }
 
 @Component({
@@ -12,14 +31,37 @@ interface Message {
 })
 export class AppComponent {
   title = 'frontend';
-  private socket$: WebSocketSubject<Message>;
+  private socket$: WebSocketSubject<Data>;
+  messages$: Observable<Message[]>;
+  groups$: Observable<Group[]>;
 
-  constructor() {
+  constructor(private sanitizer: DomSanitizer) {
     this.socket$ = webSocket('ws://localhost:8999');
-    this.socket$.subscribe(
-      message => console.log(message),
-      err => console.error(err),
-      () => console.warn('Completed!')
+    this.messages$ = this.socket$.pipe(
+      map(data =>
+        data.message
+          .split('\n')
+          .filter(log => log.length > 0)
+          .map(rawMessage => {
+            const parsedMessage = parse(rawMessage);
+            const groupMatch = parsedMessage.spans[0].text.match(/^(\S+) \|$/);
+            const group = groupMatch ? groupMatch[1] : null;
+            const groupCss = groupMatch ? parsedMessage.spans[0].css : null;
+            return {
+              group,
+              groupCss,
+              chunks: parsedMessage.spans.map(chunk => ({
+                ...chunk,
+                css: this.sanitizer.bypassSecurityTrustStyle(chunk.css)
+              }))
+            };
+          })
+      )
+    );
+    this.groups$ = this.messages$.pipe(
+      map(messages =>
+        Array.from(new Set(messages.map(message => message.group))).sort()
+      )
     );
   }
 }
